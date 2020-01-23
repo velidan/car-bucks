907f57bdf33e61e82fe8152cd156404efae1fd7d
Velidan <wellnineweb@gmail.com>
2020-01-23 14:05:47 +0200

    - completed models

--- backend/core/models.py
+++ backend/core/models.py
@@ -41,3 +41,31 @@ class FuelSubType(models.Model):
   label = models.CharField(max_length=256, help_text='A human readable label of the fuel subtype type.')
 
 
+class PaymentType(models.Model):
+  label = models.CharField(max_length=256, help_text='Type of the payment. Eg, Fuel, Service, Card wash, others')
+
+
+class Currency(models.Model):
+  code = models.CharField(max_length=3, help_text='A short currency code')
+  label = models.CharField(max_length=86, help_text='A human-readable currency label')
+
+# In general it's a fuel station, but could be a car wash/service station, etc.
+class Station(models.Model):
+  name = models.CharField(max_length=256, help_text='A station name')
+  place = models.CharField(max_length=256, help_text='A place where the station located')
+
+# The most important class.
+class Payment(models.Model):
+  car = models.ForeignKey('Car', on_delete=models.CASCADE, help_text='A car for what the payment had been done')
+  payment_type = models.ForeignKey('PaymentType', on_delete=models.CASCADE, help_text='A payment type')
+  currency = models.ForeignKey('Currency', on_delete=models.CASCADE, help_text='A currency type of the payment')
+  fuel_subtype = models.ForeignKey('FuelSubType', on_delete=models.CASCADE, null=True, help_text='Fuel subtype, 95, 92, Extra, etc.')
+  station = models.ForeignKey('Station', on_delete=models.SET_NULL, null=True, help_text='A station where payment had been made')
+
+  amount = models.IntegerField(help_text='Money amount of the payment')
+  date = models.CharField(max_length=64, help_text='A date of the payment. Should be ISO string', null=True)
+  comment = models.TextField(help_text='A payment commenct', null=True)
+  odometer = models.IntegerField(help_text='The car current odometer value when payment was made', null=True)
+  fuel_price = models.IntegerField(help_text='A fuel price. Total price can be calculated automatically if it is a Fuel payment ', null=True)
+  #  If fuel_price and this field presented - amount will be calculated automatically
+  fuel_amount = models.IntegerField(help_text='An amount of the fuel (Litre).', null=True)
